69dd30bbc4f0319c235c49dc6d46c4b197c8072b
Kashfia <kashfiabintae@gmail.com>
2024-10-25 17:23:32 +0600

    country

---
+++ Country.py
@@ -0,0 +1,31 @@
+class India:
+    def capital(self):
+        print("New Delhi is the capital of India.")
+    
+
+    def language(self):
+        print("Hindi is the most widely spoken language of India.")
+        
+    def type(self):
+        print("India is a developing country.")
+        
+    
+    
+class Bangladesh: 
+    def capital(self):
+        print("Dhaka isthe capital of Bangladesh.")
+        
+    def language(self):
+        print("Bangla is the Mother language of Bangladesh.")
+        
+    def type(self):
+        print("Bangladesh is a developing country.")
+        
+ind = India()
+bd = Bangladesh()
+
+for country in(ind, bd):
+    country.capital()
+    country.language()
+    country.type()
+    print()
